c2d8dbc9703fd05c361cc356935f86887a005c4b
mackee <macopy123@gmail.com>
2018-10-02 14:39:45 +0900

    integrate context.Context

--- interface.go
+++ interface.go
@@ -1,13 +1,23 @@
 package sqlla
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+)
 
+// DB is interface like *database/sql.DB
 type DB interface {
 	QueryRow(string, ...interface{}) *sql.Row
+	QueryRowContext(context.Context, string, ...interface{}) *sql.Row
+
 	Query(string, ...interface{}) (*sql.Rows, error)
+	QueryContext(context.Context, string, ...interface{}) (*sql.Rows, error)
+
 	Exec(string, ...interface{}) (sql.Result, error)
+	ExecContext(context.Context, string, ...interface{}) (sql.Result, error)
 }
 
+// Scanner is interface like *database/sql.Row
 type Scanner interface {
 	Scan(...interface{}) error
 }
