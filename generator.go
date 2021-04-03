56625a4b54e42023cd556324b176db9a058f9749
mackee <macopy123@gmail.com>
2021-04-03 21:22:57 +0900

    add blocker in generator. sqlla command must build by go version of 1.16 or higher

--- generator.go
+++ generator.go
@@ -1,26 +1,14 @@
 package sqlla
 
 import (
-	"bytes"
-	"go/format"
+	"fmt"
 	"io"
+	"runtime"
 	"text/template"
-
-	"github.com/pkg/errors"
 )
 
 var tmpl = template.New("table")
 
 func WriteCode(w io.Writer, table *Table) error {
-	buf := new(bytes.Buffer)
-	err := tmpl.Execute(buf, table)
-	if err != nil {
-		return errors.Wrapf(err, "fail to render")
-	}
-	bs, err := format.Source(buf.Bytes())
-	if err != nil {
-		return errors.Wrapf(err, "fail to format")
-	}
-	_, err = w.Write(bs)
-	return err
+	return fmt.Errorf("must build by go version of 1.16 or higher. this built by %s", runtime.Version())
 }
