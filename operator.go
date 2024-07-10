8ee9a6a769db056bd98253be75b5a2b2d819b4dc
mackee <macopy123@gmail.com>
2024-07-10 12:12:31 +0900

    feat: add useful comment at sqlla.Operator

--- operator.go
+++ operator.go
@@ -5,16 +5,16 @@ import (
 )
 
 var (
-	OpEqual        Operator = "="
-	OpGreater      Operator = ">"
-	OpGreaterEqual Operator = ">="
-	OpLess         Operator = "<"
-	OpLessEqual    Operator = "<="
-	OpNot          Operator = "<>"
-	OpIs           Operator = "IS"
+	OpEqual        Operator = "="  // Operator for equal. Column(value, sqlla.OpEqual) same as Column = value
+	OpGreater      Operator = ">"  // Operator for greater. Column(value, sqlla.OpGreater) same as Column > value
+	OpGreaterEqual Operator = ">=" // Operator for greater equal. Column(value, sqlla.OpGreaterEqual) same as Column >= value
+	OpLess         Operator = "<"  // Operator for less. Column(value, sqlla.OpLess) same as Column < value
+	OpLessEqual    Operator = "<=" // Operator for less equal. Column(value, sqlla.OpLessEqual) same as Column <= value
+	OpNot          Operator = "<>" // Operator for not equal. Column(value, sqlla.OpNot) same as Column <> value
+	OpIs           Operator = "IS" // Operator for is. Column(value, sqlla.OpIs) same as Column IS value
 	opIsNull       Operator = "IS NULL"
 	opIsNotNull    Operator = "IS NOT NULL"
-	OpLike         Operator = "LIKE"
+	OpLike         Operator = "LIKE" // Operator for like. Column(value, sqlla.OpLike) same as Column LIKE value
 )
 
 type Operator string
